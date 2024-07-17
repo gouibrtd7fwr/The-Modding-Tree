ab66cdaa56ca77b61079679beb2057ba3742dc50
gouibrtd7fwr <154725582+gouibrtd7fwr@users.noreply.github.com>
2024-07-17 14:45:55 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -84,7 +84,7 @@ addLayer("h", {
 addLayer("he", {
     name: "helium", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "He", // This appears on the layer's node. Default is the id with the first letter capitalized
-    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
@@ -104,8 +104,11 @@ addLayer("he", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 1, // Row the layer is in on the tree (0 is the first row)
     layerShown(){return hasUpgrade('h', 21)},
+    doReset(he) {
+        if (layers[he].row <= this.row) return;
+      },
     upgrades: {
         11: {
             title: "Finally! A boost to the worst upgrade!",
