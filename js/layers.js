932e113d149d6f27d8c52e82fa3790d22a145b41
gouibrtd7fwr <154725582+gouibrtd7fwr@users.noreply.github.com>
2026-07-01 11:13:30 +0700

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -24,5 +24,11 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+    upgrades: {
+    11: {
+        description: "Blah",
+        cost: new Decimal(100),
+    }
+}
 })
